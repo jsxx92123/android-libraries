86d78772d854de8a082b722e4a485a651a8935ad
moljac <mcvjetko@holisticware.net>
2025-04-15 21:40:11 +0200

    refactored

--- tests/extended/TestAllIndividualPackages.cs
+++ tests/extended/TestAllIndividualPackages.cs
@@ -92,18 +92,19 @@ public class TestAllIndividualPackages
 		// - Remove the target frameworks that are not 'android'
 		var proj_file = Directory.GetFiles (case_dir, "*.csproj").FirstOrDefault ();
 
-		XmlDocument xd = new ();
-		xd.Load (proj_file);
-
-		XmlNodeList nl = xd.SelectNodes("//*[starts-with(name(), 'TargetFramework')]");
-
-		foreach (XmlNode node in nl) 
-		{
-			node.InnerText = $"{net_version}-android";
-		}
-		xd.Save(proj_file);
-
 		if (proj_file is not null) {
+
+			XmlDocument xd = new ();
+			xd.Load (proj_file);
+
+			XmlNodeList nl = xd.SelectNodes("//*[starts-with(name(), 'TargetFramework')]");
+
+			foreach (XmlNode node in nl) 
+			{
+				node.InnerText = $"{net_version}-android";
+			}
+			xd.Save(proj_file);
+
 			ReplaceInFile (proj_file, ">21</SupportedOSPlatformVersion>", $">{platform_version}</SupportedOSPlatformVersion>");
 			ReplaceInFile (proj_file, ">21.0</SupportedOSPlatformVersion>", $">{platform_version}</SupportedOSPlatformVersion>");
 			ReplaceInFile (proj_file, $";{net_version}-ios", "");
