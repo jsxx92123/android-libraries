97635fa40aebdfbf605bc37f65e25c8758cb551e
moljac <mcvjetko@holisticware.net>
2025-04-19 14:12:33 +0200

    firebase-ml-vision-barcode-model fixes

---
+++ source/com.google.firebase/firebase-ml-vision-barcode-model/Additions/Google.AndroidLibraries.BarHopper.Barcode.cs
@@ -0,0 +1,9 @@
+using PointAlias=global::Android.Graphics.Point;
+
+namespace Google.AndroidLibraries.BarHopper
+{
+
+	public partial class Barcode //: global::Java.Lang.Object, global::Android.OS.IParcelable 
+	{
+	}
+}
