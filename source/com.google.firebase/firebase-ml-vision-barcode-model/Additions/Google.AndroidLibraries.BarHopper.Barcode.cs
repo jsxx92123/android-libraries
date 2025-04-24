6be36e5014c65791e343b340c47e3d77f4b57a5d
moljac <mcvjetko@holisticware.net>
2025-04-24 23:26:27 +0200

    Delete Google.AndroidLibraries.BarHopper.Barcode.cs

--- source/com.google.firebase/firebase-ml-vision-barcode-model/Additions/Google.AndroidLibraries.BarHopper.Barcode.cs
+++
@@ -1,9 +0,0 @@
-using PointAlias=global::Android.Graphics.Point;
-
-namespace Google.AndroidLibraries.BarHopper
-{
-
-	public partial class Barcode //: global::Java.Lang.Object, global::Android.OS.IParcelable 
-	{
-	}
-}
